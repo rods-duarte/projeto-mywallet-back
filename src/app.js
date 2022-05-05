dd063d9b7c4a38d7d3825b34a39a0fed276db997
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-05-05 14:35:32 -0300

    Feat: Add sign-in

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
 
-import { register } from './controllers/authController.js';
+import { login, register } from './controllers/authController.js';
 
 const app = express();
 app.use(json());
 app.use(cors());
 dotenv.config({ path: './src/config/config.env' });
 
+app.post('/sign-in', login);
 app.post('/sign-up', register);
 
 const port = process.env.PORT || 5000;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,31 @@ import db from '../database/index.js';
 
 import UserSchema from '../models/UserSchema.js';
 
+export async function login(req, res) {
+  const { email, password } = req.body;
+
+  //   if (UserSchema.validate({ email, password }).error) {
+  //     res.sendStatus(400);
+  //     return;
+  //   }
+
+  try {
+    const user = await db.collection('users').findOne({ email });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      res.sendStatus(404);
+      return;
+    }
+    const token = uuid();
+    const userId = user._id;
+    await db.collection('sessions').insertOne({ token, userId });
+    res.send({ token, userId });
+  } catch (e) {
+    console.log(e); // eslint-disable-line no-console
+    res.senStatus(500);
+  }
+}
+
 export async function register(req, res) {
   const { name, email, password, confirmPassword } = req.body;
 
